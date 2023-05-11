14992696309a99e18579fa8d7a66791594a4c516
solutionnn <127835017+solutionnn@users.noreply.github.com>
2023-05-11 15:47:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-//сюда тільки скрипти, що стосуються index.html
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        link.style.backgroundColor = '#3c3c3c';
+        link.style.transition = 'background-color 0.2s ease';
+    });
+
+    link.addEventListener('mouseout', () => {
+        link.style.backgroundColor = '#383838';
+        link.style.transition = 'background-color 0.2s ease';
+    });
+});
